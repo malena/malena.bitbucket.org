be4c10cdd461cc43af05ac1d3a1e86e3e2684c36
Malena Andrade <malena.andrade@gmail.com>
2014-06-29 17:48:33 -0400

    Adding tweenlite, scrollorama

--- site/app/static/project.js
+++ site/app/static/project.js
@@ -1,3 +1,9 @@
 var mandrade = angular.module('mandrade', []);
 
+$(document).ready(function() {
+	var controller = $.superscrollorama({
+		triggerAtCenter: false,
+		playoutAnimations: true
+	});
 
+});
