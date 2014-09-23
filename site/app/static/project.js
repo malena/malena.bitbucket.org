17dc88b22182fd9651398c4e58c0689a20e4548a
Malena Andrade <malena.andrade@gmail.com>
2014-09-23 00:39:45 -0400

    Adding in modal and tooltip

--- site/app/static/project.js
+++ site/app/static/project.js
@@ -1,4 +1,8 @@
 // initialize jQuery Scroll View 
 $(function() {
 	new MandradeScrollView({});
+
+    $('.img-pulse').on('hover', function(){
+        $('.img-pulse').tooltip('toggle');
+    });
 });
