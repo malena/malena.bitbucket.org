c54f666e099a85f6d20415626873bcd7a5f0e6ca
Malena Andrade <malena.andrade@gmail.com>
2014-11-20 00:10:47 -0500

    mobile styling, updating copy

--- app/static/js/project.js
+++ app/static/js/project.js
@@ -1,18 +1,18 @@
 // initialize jQuery Scroll View 
 $(function() {
-	new MandradeScrollView();
-
-/*
 	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
  		// some code..
 	}
 	else {
+		new MandradeScrollView();
+
+		/*
 		if (document.documentElement.clientWidth > 600) {
 		    $(window).on('beforeunload', function() {
 		        //$(window).scrollTop(0); 
 		    });
 		};
+		*/
 	}
-*/
 
 });
