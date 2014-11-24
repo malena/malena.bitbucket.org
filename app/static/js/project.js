7e3c0b54c8f84202eb7017986019a23b906d6e15
Malena Andrade <malena.andrade@gmail.com>
2014-11-23 20:26:10 -0500

    Rotate panel1 image for devices with no js

--- app/static/js/project.js
+++ app/static/js/project.js
@@ -2,4 +2,7 @@ $(function() {
 	if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		new MandradeScrollView();
 	} 
+	$('img.img-malena').css('display', 'block');
+	$('img.img-malena').css('transform', 'rotate(-30deg)');
+
 });
