334614433e187f0dc2195e998cc676f155046db4
Malena Andrade <malena.andrade@gmail.com>
2015-02-22 15:29:27 -0500

    updating mobile styles

--- app/static/js/project.js
+++ app/static/js/project.js
@@ -16,5 +16,4 @@ $(function() {
 	if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		new MandradeScrollView();
 	} 
-	$('img.img-malena').css('display', 'block');
 });
